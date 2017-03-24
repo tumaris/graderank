0c2b6c6e2c6bcbf889d75c7334a36fb00ec2c7f6
Minh Le <lenguyetminh1998@gmail.com>
2017-03-24 19:17:31 -0400

    Testing conversion of SQL table to JSON format

---
+++ file.php
@@ -0,0 +1,11 @@
+<?php
+  $connect = mysqli_connect("localhost", "root", "", "graderank");
+  $sql = "SELECT * from score2";
+  $result = mysqli_query($connect, $sql);
+  $json_array = array();
+  while($row = mysqli_fetch_assoc($result)){
+    $json_array[] = $row;
+  }
+
+  echo json_encode($json_array);
+?>
