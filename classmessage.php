9054e7b299fc9fa0210ee3268897f1a00893e4ee
Minh Le <lenguyetminh1998@gmail.com>
2017-03-27 07:44:16 -0400

    Add in function to submit grade
    Add in function to submit grade straight from class page

--- classmessage.php
+++ classmessage.php
@@ -6,6 +6,34 @@
   }
 
   date_default_timezone_set('America/New_York');
+
+  //Adding Grades to Class Data
+  if (isset($_POST['gradeSubmit'])){
+    $letter = mysql_real_escape_string($_POST['letter']);
+
+    if ($letter == "A" || $letter == "A+") $letter = 4;
+    if ($letter == "A-") $letter = 3.67;
+    if ($letter == "B+") $letter = 3.33;
+    if ($letter == "B") $letter = 3;
+    if ($letter == "B-") $letter = 2.67;
+    if ($letter == "C+") $letter = 2.33;
+    if ($letter == "C")$letter = 2;
+    if ($letter == "C-") $letter = 1.67;
+    if ($letter == "D+") $letter = 1.33;
+    if ($letter == "D")$letter = 1;
+    if ($letter == "F")$letter = 0;
+
+      $sql = "INSERT INTO score2(class, grade)". "VALUES ('CS171', '$letter')";
+      header("Location: CS171.php");
+
+      if ($conn->query($sql)===true){
+        $_SESSION['message'] = "Registration successful.";
+      }
+
+  }
+
+
+
   function setComments($conn)
   {
     if (isset($_POST['commentSubmit'])){
@@ -32,9 +60,10 @@
         echo '<b>Date posted: </b>'. $row['date']."<br>";
         echo '<b> Review: </b>' . $row['message'];
       echo "</div>";
-      echo "</div>"; 
+      echo "</div>";
 
     }
   }
+
 
 ?>
